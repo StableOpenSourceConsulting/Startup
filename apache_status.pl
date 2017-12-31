852a78b512454466456731153ba92c9028e42dcd
Chris Smith <chris.smith@cgl.org.uk>
2017-12-31 18:38:02 +0000

    new file:   apache_status.pl -Script to capture active workers from apache status. Idela for monitoring ;o)

---
+++ apache_status.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+# uses output from apache server-status to monitor how many apache worker processes are busy and Requests Per Second
+# cs 6/6/2017
+use strict;
+use warnings;
+use ADroutines;
+
+my $port = IS_get_server_name() eq 'Endive' ? qq{:7080\\} : '';
+my $domain  = IS_get_server_name() eq 'Endive' ? 'criisrestore' : 'criis';
+
+my $apache_status = "lynx -auth=Stats4U\$\:'CR!!S4pache5tat54U\$\' -dump http:\/\/localhost\$port/server-status?auto";
+my @apache_stats=split(/\s/,`$apache_status`); # Use space to separate array elements. Makes it easier to select numbers later
+chomp @apache_stats;
+
+# Print out all stats to a file for zabbix to monitor
+open(my $fh, ">", "/var/tmp/apache_status.txt");
+my @apache_op=split(/\n/,`$apache_status`);
+for (@apache_op) {
+  print $fh "$_\n";
+}
+close $fh;
+
+my $ReqPerSec   = $apache_stats[11];
+my $BusyWorkers = $apache_stats[17];
+
+if ($ReqPerSec >= 27.1 || $BusyWorkers >= 200) { 
+  my $status_message = "apache-status.pl : " . IS_get_server_name() . " reports high values -> Request Per Second: $ReqPerSec Busy Workers: $BusyWorkers. See http://www.$domain.org.uk/server-status";
+  my $hour = (localtime)[2];
+  if ($hour >= 7 && $hour <= 19) {
+    IS_slack_notify($status_message, 0, IS_get_server_name() . "  Apache Process status");
+  }
+}
