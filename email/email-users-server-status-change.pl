b61daf93f078b6425da62e76d6d241b2cc433998
Chris Smith <@chrisleighsmith>
2019-01-12 14:26:21 +0000

    Delete Blank Lines

--- email/email-users-server-status-change.pl
+++ email/email-users-server-status-change.pl
@@ -4,13 +4,12 @@
 #
  Note: This script will email 3000 users  with a default messages "There has been issues today on CRiiS...." and takes about 8 mins to complete
 # Full details of remote script and message are available from customerlane.org.uk /var/www/customerdoamin/dr_scripts/email_alert_criis_users.pl
-#
+
 #Author:Date:Revision 1.0  
 #Chris Smith:20170124:01
 
 use strict;
 use warnings;
-
 
 print "Note: This script sends approx 3,000 Emails to all users with a message defined in customer-domain.com:/var/www/customerdoamin.com/dr_scripts/message.txt\n";
 print "Are you sure you want to continue Y/n ? ";
